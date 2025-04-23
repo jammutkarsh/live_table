1e2bf130d2d703c873b6c550932dadd62ae2cfdd
cvkmohan <krishna@gurujada.com>
2025-04-23 20:44:06 +0530

    Restore remote version of files outside demo folder

---
+++ priv/repo/migrations/20241223113523_create_products.exs
@@ -0,0 +1,14 @@
+defmodule LiveTable.Repo.Migrations.CreateProducts do
+  use Ecto.Migration
+
+  def change do
+    create table(:products) do
+      add :name, :string
+      add :description, :text
+      add :price, :decimal
+      add :stock_quantity, :integer
+
+      timestamps(type: :utc_datetime)
+    end
+  end
+end
